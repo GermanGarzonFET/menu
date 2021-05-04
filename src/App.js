7a9d087e04a66f9d7a4b1b7cb7b584a4cbb44a99
GermanGarzonFET <53948926+GermanGarzonFET@users.noreply.github.com>
2021-05-04 12:47:43 -0500

    menu example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import SideBar from './menu'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+
+      <div id="page-wrap">
+        <h1>Cool Restaurant 🍔🍕</h1>
+        <h2>Check out our offerings in the sidebar!</h2>
+      </div>
     </div>
   );
 }
